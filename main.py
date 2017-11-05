a000740c6a1d479e1365be81a7518d36807578ad
mike alvarino <michael.alvarino@gmail.com>
2017-11-05 10:22:24 -0500

    remove old data.py

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import preprocessing
 
 
 def main():
     fname = "data/train.csv"
     data = preprocessing.get_data(fname)
-    j = data[['pickup_longitude', 'pickup_latitude']].as_matrix()
-    k = data[['dropoff_longitude', 'dropoff_latitude']].as_matrix()
-    z = np.concatenate((j, k))
-    sns.jointplot(x=z[:, 0], y=z[:, 1])
-    plt.show()
+    print(data.describe())
 
 
 def build_heatmap(dataset):
