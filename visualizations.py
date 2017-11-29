a91a2bb514d4cd9878c57872231a5421a2cf3bd6
mike alvarino <michael.alvarino@gmail.com>
2017-11-28 21:55:32 -0500

    working polynomial visualization

--- visualizations.py
+++ visualizations.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import data
+import basis_functions
 import matplotlib.pyplot as plt
 
 
@@ -29,6 +30,25 @@ def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb, qalpha):
     x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
         .apply(lambda x: pd.to_datetime(x, unit="s").hour)
     x_vis_stndzd = data.standardize_cols(x_vis).fillna(0.0)
+    qw_i = qw.sample().eval()
+    qb_i = qb.sample().eval()
+    qalpha_i = qalpha.sample().eval()
+    line = np.matmul(x_vis_stndzd, qw_i) + qb_i + qalpha_i
+    plt.scatter(x=times, y=line)
+    plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
+    plt.xlabel("Pickup Hour")
+    plt.ylabel("Trip Duration")
+    plt.show()
+
+
+def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb, qalpha):
+    times = np.linspace(0, 24, num_pnts)
+    x_vis = pd.DataFrame({i: [0.0] * num_pnts for i in indicator_cols})
+    x_vis["pickup_hour"] = times
+    x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
+        .apply(lambda x: pd.to_datetime(x, unit="s").hour)
+    x_vis_stndzd = basis_functions.polynomial(
+        data.standardize_cols(x_vis).fillna(0.0), degree=degree)
     qw_i = qw.sample().eval()
     qb_i = qb.sample().eval()
     qalpha_i = qalpha.sample().eval()
