a2a78eb0cbc8d262823705b0bf4081fe5f167666
mike alvarino <michael.alvarino@gmail.com>
2017-11-28 21:29:07 -0500

    completed linear model

--- visualizations.py
+++ visualizations.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
+import data
+import matplotlib.pyplot as plt
 
 
 def visualize_by_borough(dataset):
@@ -17,3 +20,21 @@ def visualize_by_borough(dataset):
                       hue="neighborhood",
                       data=data,
                       fit_reg=False)
+
+
+def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb, qalpha):
+    times = np.linspace(0, 24, num_pnts)
+    x_vis = pd.DataFrame({i: [0.0] * num_pnts for i in indicator_cols})
+    x_vis["pickup_hour"] = times
+    x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
+        .apply(lambda x: pd.to_datetime(x, unit="s").hour)
+    x_vis_stndzd = data.standardize_cols(x_vis).fillna(0.0)
+    qw_i = qw.sample().eval()
+    qb_i = qb.sample().eval()
+    qalpha_i = qalpha.sample().eval()
+    line = np.matmul(x_vis_stndzd, qw_i) + qb_i + qalpha_i
+    plt.scatter(x=times, y=line)
+    plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
+    plt.xlabel("Pickup Hour")
+    plt.ylabel("Trip Duration")
+    plt.show()
