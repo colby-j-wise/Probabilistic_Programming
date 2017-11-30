ce9aedecb417f26baf77da75113459335bf148f1
mike alvarino <michael.alvarino@gmail.com>
2017-11-29 19:35:04 -0500

    minor edits to wording/spelling and better visualizations

--- visualizations.py
+++ visualizations.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import data
 import basis_functions
 import matplotlib.pyplot as plt
+import edward as ed
+import tensorflow as tf
 
 
 def visualize_by_borough(dataset):
@@ -23,17 +25,18 @@ def visualize_by_borough(dataset):
                       fit_reg=False)
 
 
-def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb, qalpha):
+def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb):
     times = np.linspace(0, 24, num_pnts)
     x_vis = pd.DataFrame({i: [0.0] * num_pnts for i in indicator_cols})
     x_vis["pickup_hour"] = times
     x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
         .apply(lambda x: pd.to_datetime(x, unit="s").hour)
     x_vis_stndzd = data.standardize_cols(x_vis).fillna(0.0)
-    qw_i = qw.sample().eval()
-    qb_i = qb.sample().eval()
-    qalpha_i = qalpha.sample().eval()
-    line = np.matmul(x_vis_stndzd, qw_i) + qb_i + qalpha_i
+    qw_i = tf.constant(qw.sample().eval(), tf.float64)
+    qb_i = tf.constant(qb.sample().eval(), tf.float64)
+    line = ed.models.Normal(loc=ed.dot(x_vis_stndzd, qw_i)
+                            + qb_i, scale=tf.constant([1.0], tf.float64))\
+        .sample().eval()
     plt.scatter(x=times, y=line)
     plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
     plt.xlabel("Pickup Hour")
@@ -41,7 +44,7 @@ def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb, qalpha):
     plt.show()
 
 
-def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb, qalpha):
+def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb):
     times = np.linspace(0, 24, num_pnts)
     x_vis = pd.DataFrame({i: [0.0] * num_pnts for i in indicator_cols})
     x_vis["pickup_hour"] = times
@@ -49,9 +52,11 @@ def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb, qalpha):
         .apply(lambda x: pd.to_datetime(x, unit="s").hour)
     x_vis_stndzd = basis_functions.polynomial(
         data.standardize_cols(x_vis).fillna(0.0), degree=degree)
-    qw_i = qw.sample().eval()
-    qb_i = qb.sample().eval()
-    qalpha_i = qalpha.sample().eval()
+    qw_i = tf.constant(qw.sample().eval(), tf.float64)
+    qb_i = tf.constant(qb.sample().eval(), tf.float64)
+    line = ed.models.Normal(loc=ed.dot(x_vis_stndzd, qw_i)
+                            + qb_i, scale=tf.constant([1.0], tf.float64))\
+        .sample().eval()
     line = np.matmul(x_vis_stndzd, qw_i) + qb_i + qalpha_i
     plt.scatter(x=times, y=line)
     plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
