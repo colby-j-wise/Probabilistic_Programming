97924341eac46a152fd8146feddc2e1ff0c45278
mike alvarino <michael.alvarino@gmail.com>
2017-11-30 18:59:25 -0500

    combine glm and poly, fix visualizations to one sample of qw

--- visualizations.py
+++ visualizations.py
@@ -32,17 +32,14 @@ def vis_glm(num_pnts, indicator_cols, actual_data, qw, qb):
     x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
         .apply(lambda x: pd.to_datetime(x, unit="s").hour)
     x_vis_stndzd = data.standardize_cols(x_vis).fillna(0.0)
-    qw_i = tf.constant(qw.sample().eval(), tf.float64)
-    qb_i = tf.constant(qb.sample().eval(), tf.float64)
-    line = ed.models.Normal(loc=ed.dot(x_vis_stndzd, qw_i) +
-                            qb_i, scale=tf.constant([1.0], tf.float64))\
-        .sample().eval()
+    qw_i = qw.sample().eval()
+    qb_i = qb.sample().eval()
+    line = np.dot(x_vis_stndzd, qw_i) + qb_i
     plt.scatter(x=times, y=line)
     plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
     plt.xlabel("Pickup Hour")
     plt.ylabel("Trip Duration")
     plt.show()
-
 
 def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb):
     times = np.linspace(0, 24, num_pnts)
@@ -51,12 +48,10 @@ def vis_glm_poly(num_pnts, degree, indicator_cols, actual_data, qw, qb):
     x_vis["pickup_timestamp"] = x_vis["pickup_hour"]\
         .apply(lambda x: pd.to_datetime(x, unit="s").hour)
     x_vis_stndzd = basis_functions.polynomial(
-        data.standardize_cols(x_vis).fillna(0.0), degree=degree)
-    qw_i = tf.constant(qw.sample().eval(), tf.float64)
-    qb_i = tf.constant(qb.sample().eval(), tf.float64)
-    line = ed.models.Normal(loc=ed.dot(x_vis_stndzd, qw_i) +
-                            qb_i, scale=tf.constant([1.0], tf.float64))\
-        .sample().eval()
+    data.standardize_cols(x_vis).fillna(0.0), degree=degree)
+    qw_i = qw.sample().eval()
+    qb_i = qb.sample().eval()
+    line = np.dot(x_vis_stndzd, qw_i) + qb_i
     plt.scatter(x=times, y=line)
     plt.scatter(x=actual_data["pickup_hour"], y=actual_data["trip_duration"])
     plt.xlabel("Pickup Hour")
