1e665563b45c0d426024340b9cd13f606416d265
mike alvarino <michael.alvarino@gmail.com>
2017-11-30 19:10:29 -0500

    add back rbf

--- kernels.py
+++ kernels.py
@@ -13,7 +13,28 @@ def is_positive_definite(X):
 		return False
 
 
-def RationalQuadratic(X, X2=None, lengthScale=1.0, alpha=1.0): 
+def rbf(X, X2=None, lengthscale=1.0, variance=1.0):
+    lengthscale = tf.convert_to_tensor(lengthscale, tf.float64)
+    variance = tf.convert_to_tensor(variance, tf.float64)
+
+    X = tf.convert_to_tensor(X)
+    X = X / lengthscale
+    Xs = tf.reduce_sum(tf.square(X), 1)
+    if X2 is None:
+        X2 = X
+        X2s = Xs
+    else:
+        X2 = tf.convert_to_tensor(X2)
+        X2 = X2 / lengthscale
+        X2s = tf.reduce_sum(tf.square(X2), 1)
+
+    square = tf.reshape(Xs, [-1, 1]) + tf.reshape(X2s, [1, -1]) - \
+        2 * tf.matmul(X, X2, transpose_b=True)
+    output = variance * tf.exp(-square / 2)
+    return output
+
+
+def RationalQuadratic(X, X2=None, lengthScale=1.0, alpha=1.0):
     lengthScale = tf.convert_to_tensor(lengthScale, tf.float64)
     alpha = tf.convert_to_tensor(alpha, tf.float64)
     X = tf.convert_to_tensor(X)
@@ -28,12 +49,8 @@ def RationalQuadratic(X, X2=None, lengthScale=1.0, alpha=1.0):
         X2 = X2 / alpha
         X2 = X2 / lengthScale
         X2s = tf.reduce_sum(tf.square(X2), 1)
-    square = tf.reshape(Xs, [-1, 1]) + tf.reshape(X2s, [1, -1]) - 2 * tf.matmul(X, X2, transpose_b=True)
+    square = tf.reshape(Xs, [-1, 1]) + tf.reshape(X2s, [1, -1]) - 2 *\
+        tf.matmul(X, X2, transpose_b=True)
     output =  1 + (square / 2)
     K = tf.pow(output, -alpha)
     return K
-
-
-
-
-
